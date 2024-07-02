4d9c024b8accf055263abce2b53a22acb45e9558
euuuuuuan <euan.may24@gmail.com>
2024-07-02 17:43:34 +0900

    [Feat] Blog 페이지 댓글 시스템 구현

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
+
+# 가상의 데이터베이스
+comments = []
 
 @app.route('/')
 def home():
@@ -12,7 +15,7 @@ def about():
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html')
+    return render_template('blog.html', comments=comments)
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -20,9 +23,16 @@ def contact():
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
-        # 여기에 이메일 보내기 로직 또는 데이터베이스 저장 로직을 추가 가능
+        # 이메일 보내기 로직 또는 데이터베이스 저장 로직 추가 가능
         return redirect(url_for('home'))
     return render_template('contact.html')
 
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+    comment = request.json['comment']
+    comments.append(comment)
+    return jsonify({'status': 'success', 'comment': comment})
+
 if __name__ == '__main__':
     app.run(debug=True)
+
