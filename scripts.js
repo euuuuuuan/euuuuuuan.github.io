04e3dd36f734191ecdac22c51550af9a7d3619e0
euuuuuuan <euan.may24@gmail.com>
2024-07-02 17:39:08 +0900

    [Feat] 동적 쿼리 구현

--- scripts.js
+++ scripts.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
 });
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const navbarToggler = document.querySelector(".navbar-toggler");
-//     const navbarCollapse = document.querySelector("#navbarNav");
-//
-//     navbarToggler.addEventListener("click", function() {
-//         navbarCollapse.classList.toggle("show");
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector("#navbarNav");
+
+    navbarToggler.addEventListener("click", function() {
+        navbarCollapse.classList.toggle("show");
+    });
+});
